836cb5e2f5ade566d0c0bd2d2edd710e6f25419b
Alexis Rojas <alexRoJas_199@hotmail.com>
2022-06-26 19:09:15 -0500

    Subida con app de vue

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const app = express()
-const hbs = require('hbs');
+// const hbs = require('hbs');
 require('dotenv').config();
 
 const port = process.env.PORT 
 
-hbs.registerPartials(__dirname + '/views/partials');
+// hbs.registerPartials(__dirname + '/views/partials');
 
-app.set('view engine', 'hbs');
+// app.set('view engine', 'hbs');
 
 app.use(express.static('public'))
 
-app.get('/', (req,res) =>{
-    res.render('home', {
-        nombre: 'Alexis',
-        titulo: 'Home'
-    })
-})
+// app.get('/', (req,res) =>{
+//     res.render('home', {
+//         nombre: 'Alexis',
+//         titulo: 'Home'
+//     })
+// })
 
 app.get('*', (req,res) =>{
-    res.send('404 | pagina no encontrada')
+    res.send(__dirname+'/public/index.html')
 })
 
 
